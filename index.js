86e7cc909f49bb652e891c5fab66e2fcaff1c099
LOI DINH <loiphatdinh@gmail.com>
2018-03-25 21:35:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return `There is nobody waiting to be served.`;
+  }
+  else {
+    var first = "";
+    first = katzDeliLine.slice(1);
+    return first;
+  }
+}
