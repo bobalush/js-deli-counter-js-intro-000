b6f7ca0d7380cd5c8485dd1e94a0b09254a92650
LOI DINH <loiphatdinh@gmail.com>
2018-03-25 21:27:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine, name) {
   var i = 0;
   while (i < katzDeliLine.length) {
     if (katzDeliLine[i] === name) {
-      return `Welcome, ${name}. You are number ${i} in line.`;
+      return `Welcome, ${name}. You are number ${i + 1} in line.`;
     }
     i += 1;
   }
